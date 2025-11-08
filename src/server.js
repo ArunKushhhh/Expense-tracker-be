52377bb3638f6c6fdcded57b6fd84d86a102fa29
Arun Kumar Kushwaha <arunkushwaha_230604@aitpune.edu.in>
2025-11-09 02:16:34 +0530

    added/cron-for-production

--- src/server.js
+++ src/server.js
@@ -4,14 +4,19 @@ import express from "express";
 import rateLimiter from "./middleware/rateLimiter.js";
 import TransactionsRoute from "./routes/transactionsRoute.js";
 import { sql } from "./config/db.js";
+import job from "./config/cron.js";
 
 const app = express();
+
+if (process.env.NODE_ENV === "production") job.start();
 
 //middlewares
 app.use(rateLimiter);
 app.use(express.json());
 
 const port = process.env.PORT || 8000;
+
+app.get("/api/health", (req, res) => [res.status(200).json({ status: "OK" })]);
 
 async function initDB() {
   try {
